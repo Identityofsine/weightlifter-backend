c862a2a0ff94b1a67ce928fda3890bb3e561a231
fofx <identityofsine@gmail.com>
2024-06-17 15:35:42 -0400

    database

--- database.sql
+++ database.sql
@@ -4,55 +4,91 @@ CREATE USER 'weightlifter'@'%' IDENTIFIED BY 'weightlifter';
 GRANT ALL PRIVILEGES ON weightlifter.* TO 'weightlifter'@'%';
 FLUSH PRIVILEGES;
 
-USE coinbank;
-
-CREATE TABLE `transactions` (
-  `transaction_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `coinbank_id` integer NOT NULL,
-  `user_id` integer, -- why? this is because the coinjar can be shared with multiple users; it is impossible (as of now) to know who inserted the coin until the user claims it to be them.
-	`value` float NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+USE weightlifter;
 
 CREATE TABLE `user` (
   `user_id` integer PRIMARY KEY NOT NULL, 
   `username` VARCHAR(40) NOT NULL UNIQUE,
   `password` VARCHAR(225) NOT NULL,
-	`active_token` VARCHAR(225),
-	`refresh_token` VARCHAR(225) 
+	`nfc_key` VARCHAR(225) NOT NULL,
+	`permission` INTEGER NOT NULL DEFAULT 0, -- 0 = user, 1 = workout_mutate, 2 = admin 
+	`created_at` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `coinbank` (
-  `coinbank_id` integer PRIMARY KEY NOT NULL,
-	`name` VARCHAR(40) NOT NULL,
-	`emoji` VARCHAR(40),
-  `value` float NOT NULL,
-	`token` VARCHAR(225) NOT NULL,
-	`user_secret` VARCHAR(40) NOT NULL UNIQUE, -- This is the secret that the user will have to input to add themselves to the coinbank.
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `workout` (
+  `workout_id` integer PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE `coinbank_user_bridge` (
-  `cub_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `coinbank_id` integer NOT NULL,
-  `user_id` integer NOT NULL
+CREATE TABLE `workout_bridge` (
+  `workout_bridge_id` integer PRIMARY KEY NOT NULL,
+	`workout_id` integer NOT NULL,
+	`exercise_id` integer NOT NULL,
+	`sets` integer NOT NULL,
+	`reps` integer NOT NULL,
+	`order` integer NOT NULL
 );
 
-CREATE TABLE `coinbank_auth_users` (
-	`auth_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	`coinbank_id` integer NOT NULL,
-	`user_id` integer NOT NULL
-	-- is_owner boolean NOT NULL DEFAULT 0
+CREATE TABLE `exercise` (
+  `exercise_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`description` TEXT,
+	`sets` INTEGER NOT NULL DEFAULT 0,
+	`reps` INTEGER NOT NULL DEFAULT 0,
+	`time_flag` BOOLEAN NOT NULL DEFAULT 0
 );
 
-ALTER TABLE `transactions` ADD FOREIGN KEY (`coinbank_id`) REFERENCES `coinbank` (`coinbank_id`);
+CREATE TABLE `user_workout_history` (
+	`uwh_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`user_id` integer NOT NULL,
+	`workout_id` integer NOT NULL,
+);
 
-ALTER TABLE `transactions` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+CREATE TABLE `past_workouts` (
+	`pw_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`workout_id` integer NOT NULL,
+	`finished` datetime,
+	`duration` integer
+);
 
-ALTER TABLE `coinbank_user_bridge` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+CREATE TABLE `past_exercise` (
+	`pe_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`uwh_id` integer NOT NULL,
+	`workout_id` integer NOT NULL,
+	`exercise_id` integer NOT NULL,
+	`sets` integer NOT NULL DEFAULT 0,
+	`reps` integer NOT NULL DEFAULT 0
+);
 
-ALTER TABLE `coinbank_user_bridge` ADD FOREIGN KEY (`coinbank_id`) REFERENCES `coinbank` (`coinbank_id`);
+CREATE TABLE `measurements` (
+	`measurement_id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`user_id` integer NOT NULL,
+	`weight` integer NOT NULL,
+	`bodyfat` float NOT NULL,
+	`neck` float NOT NULL,
+	`back` float NOT NULL,
+	`chest` float NOT NULL,
+	`shoulders` float NOT NULL,
+	`waist` float NOT NULL,
+	`left_arm` float NOT NULL,
+	`right_arm` float NOT NULL,
+	`left_forearm` float NOT NULL,
+	`right_forearm` float NOT NULL,
+	`left_quad` float NOT NULL,
+	`right_quad` float NOT NULL,
+	`date` datetime NOT NULL
+);
 
-ALTER TABLE `coinbank_auth_users` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+ALTER TABLE `workout_bridge` ADD FOREIGN KEY (`workout_id`) REFERENCES `workout`(`workout_id`);
+ALTER TABLE `workout_bridge` ADD FOREIGN KEY (`exercise_id`) REFERENCES `exercise`(`exercise_id`);
 
-ALTER TABLE `coinbank_auth_users` ADD FOREIGN KEY (`coinbank_id`) REFERENCES `coinbank` (`coinbank_id`);
+ALTER TABLE `user_workout_history` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`);
+ALTER TABLE `user_workout_history` ADD FOREIGN KEY (`workout_id`) REFERENCES `past_workouts`(`workout_id`);
+
+ALTER TABLE `past_workouts` ADD FOREIGN KEY (`workout_id`) REFERENCES `workout`(`workout_id`);
+
+ALTER TABLE `past_exercise` ADD FOREIGN KEY (`uwh_id`) REFERENCES `user_workout_history`(`uwh_id`);
+ALTER TABLE `past_exercise` ADD FOREIGN KEY (`workout_id`) REFERENCES `past_workouts`(`workout_id`);
+ALTER TABLE `past_exercise` ADD FOREIGN KEY (`exercise_id`) REFERENCES `exercise`(`exercise_id`);
+
+ALTER TABLE `measurements` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`);
+
